574b7206a65ce4d32f7c849e53be65bce4a2ae3f
kezzyj <karachidavid31@gmail.com>
2022-04-07 09:46:36 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,9 @@
 #include<stdio.h>
 
 /*
- *
- * 	 * This is the preferred style for multi-line
- *
- * 	 	 * comments in C source code.
- *
- * 	 	 	 * Please use it consistently.
- *
- * 	 	 	 	 *
- *
- * 	 	 	 	 	 * Description:  A column of asterisks on the left side,
- *
- * 	 	 	 	 	 	 * with beginning and ending almost-blank lines.
- *
- * 	 	 	 	 	 	 	 */
+ *	Description:  A column of asterisks on the left side,
+ *	with beginning and ending almost-blank lines.
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
