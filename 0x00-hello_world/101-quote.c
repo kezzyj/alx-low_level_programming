5024e73ec65e54c6c17637193bceda8d738b0cb6
kezzyj <karachidavid31@gmail.com>
2022-04-08 09:11:31 -0700

    printing to error file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 int main(void)
 
 {
-	char var[} = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+	char var[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
 	write(2, var, 59);
 	return (1);
 }
