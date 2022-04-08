91d9a76e16ad8344ab4fdd046f810c4c2bd76932
kezzyj <karachidavid31@gmail.com>
2022-04-08 05:55:31 -0700

    print with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
-/*
- *	Description:  A column of asterisks on the left side,
- *	with beginning and ending almost-blank lines.
+/**
+ *	Description: main - prints out a string using prinf,
+ *	Return: 0 if success.
  */
+
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
