8fe072d3b4fb21d2e0abb27c016dd0153ac0dcbe
kezzyj <karachidavid31@gmail.com>
2022-04-08 09:09:25 -0700

    printing to error file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*	Description: main - print to the standard error
+*	Return: 1 if successful
+*/
+
+int main(void)
+
+{
+	char var[} = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+	write(2, var, 59);
+	return (1);
+}
