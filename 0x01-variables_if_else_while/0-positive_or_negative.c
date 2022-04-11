55d9d06808351a2fe05de35e555c7ae74a3ebd9a
kezzyj <karachidavid31@gmail.com>
2022-04-11 04:47:08 -0700

    printing with if and else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
 *	Description: main - this prints positive and negative integers,
@@ -13,7 +14,7 @@ int main(void)
 
 
 	 srand(time(0));
-	 n = rand() - RANDMAX / 2;
+	 n = rand() - RAND_MAX / 2;
 
 	 if(n > 0)
 	 {
