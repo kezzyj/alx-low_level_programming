850a5b717237d283498b3281bcc3b5c629d8d1b9
kezzyj <karachidavid31@gmail.com>
2022-04-11 09:08:07 -0700

    print single intergers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*	Description: main - prints integers from 0 to 9,
+*	Return: 0 if success
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
