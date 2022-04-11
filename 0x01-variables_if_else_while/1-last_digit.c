c0da608b9c870f4887bdace341d91cc26f072dc5
kezzyj <karachidavid31@gmail.com>
2022-04-11 06:51:28 -0700

    printing with comparisson 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,6 +10,7 @@
 int main(void)
 {
 	int n, lastDigit;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastDigit = n % 10;
@@ -17,13 +18,11 @@ int main(void)
 	if (lastDigit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-	}
-	
+	}	
 	else if (lastDigit == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
-	}
-	
+	}	
 	else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
