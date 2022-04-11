ed43eed7ce87b2695fcb940a2bbd49bf0832ec1b
kezzyj <karachidavid31@gmail.com>
2022-04-11 14:13:51 -0700

    print numbers in reverse

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*	Description: main - prints single numbers
+*	with space and coma,
+*	Return: 0 on success
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 58; i >= 48; i--)
+	{
+		putchar(i);
+		putchar(', ');
+	}
+	putchar('\n');
+	return (0);
+}
