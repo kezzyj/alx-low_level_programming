efe8344d2d522153f3be796beb5f6215e7f6bc78
kezzyj <karachidavid31@gmail.com>
2022-04-13 08:35:55 -0700

    printing hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -22,24 +22,20 @@ void jack_bauer(void)
 		if (h2 == 2)
 		{
 			/*Restrain to 23h, not 29*/
-
 			a = 3;
 		}
 		h1 = 0;
 
 		while (h1 <= a)
 		{
-
 			m2 = 0;
 
 			while (m2 <= 5)
 			{
-
 				m1 = 0;
 
 				while (m1 <= 9)
 				{
-
 					_putchar('0' + h2);
 					_putchar('0' + h1);
 					_putchar(':');
