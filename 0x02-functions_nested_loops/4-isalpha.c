e9f36e22d7a721be40627204cba8ad1caf532c85
kezzyj <karachidavid31@gmail.com>
2022-04-13 03:41:36 -0700

    check letter or not

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*	Description: int _isalpha - function to check if
+*	letter is alpha
+*	@c:Variable to check
+*	Return: 1 on success
+*/
+
+int _isalpha(int c)
+{
+	return (c >= 65 && c <= 90 || c >= 97 && c <= 122);
+}
