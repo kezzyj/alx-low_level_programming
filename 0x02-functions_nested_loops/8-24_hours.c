695c17e99c93f209b6f9c62cc023041ea83c429a
kezzyj <karachidavid31@gmail.com>
2022-04-13 08:13:22 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,9 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * jack_bauer - prints every minute of the day of Jack Bauer
- *
- *   * Return:void
- *
- *    */
+* jack_bauer - prints every minute of the day of Jack Bauer
+* Return:void
+*/
 
 
 
@@ -36,59 +31,59 @@ void jack_bauer(void)
 
 	{
 
-			if (h2 == 2)
+		if (h2 == 2)
 
-					{
+		{
 
-								/*Restrain to 23h, not 29*/
+			/*Restrain to 23h, not 29*/
 
-								a = 3;
+			a = 3;
 
-									}
+		}
 
-				h1 = 0;
+		h1 = 0;
 
-					while (h1 <= a)
+		while (h1 <= a)
 
-							{
+		{
 
-										m2 = 0;
+			m2 = 0;
 
-												while (m2 <= 5)
+			while (m2 <= 5)
 
-															{
+			{
 
-																			m1 = 0;
+				m1 = 0;
 
-																						while (m1 <= 9)
+				while (m1 <= 9)
 
-																										{
+				{
 
-																															_putchar('0' + h2);
+					_putchar('0' + h2);
 
-																																			_putchar('0' + h1);
+					_putchar('0' + h1);
 
-																																							_putchar(':');
+					_putchar(':');
 
-																																											_putchar('0' + m2);
+					_putchar('0' + m2);
 
-																																															_putchar('0' + m1);
+					_putchar('0' + m1);
 
-																																																			_putchar('\n');
+					_putchar('\n');
 
-																																																							m1++;
+					m1++;
 
-																																																										}
+				}
 
-																									m2++;
+				m2++;
 
-																											}
+			}
 
-														h1++;
+			h1++;
 
-															}
+		}
 
-						h2++;
+		h2++;
 
 	}
 
