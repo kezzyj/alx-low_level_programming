2314db2e83036e02af482174bc32cca46d964647
kezzyj <karachidavid31@gmail.com>
2022-04-15 05:21:57 -0700

    printitng integers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_number - Prints an integer.
+* @n: The integer to be printed.
+*/
+
+void print_number(int n)
+
+{
+	if (n < 0)
+		_putchar(-n);
+	
+	_putchar(n);
+}
