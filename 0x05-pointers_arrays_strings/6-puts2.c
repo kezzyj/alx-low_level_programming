a79b1677605ad7130491fa4b65cea4921feee106
kezzyj <karachidavid31@gmail.com>
2022-04-20 09:15:54 -0700

    print every other number

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* puts2 - Prints one char out of two of a string.
+* @str: The string containing characters.
+*/
+
+void puts2(char *str)
+{
+	int index;
+
+	for (index = 0; index < len; index += 2)
+
+		_putchar(str[index]);
+
+
+
+	_putchar('\n');
+
+}
