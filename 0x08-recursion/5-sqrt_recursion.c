45dd7d1ad576c38edf8106c32108d361fb39cf14
kezzyj <karachidavid31@gmail.com>
2022-04-28 06:14:11 -0700

    sqaure root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -22,16 +22,15 @@ int find_sqrt(int num, int root)
 }
 
 /**
- * find_sqrt - Finds the natural square root of an inputted number.
- * @num: The number to find the square root of.
- * @root: The root to be tested.
+ * _sqrt_recursion - Finds the natural square root of an inputted number.
+ * @n: The number to find the square root of.
  * Return: If the number has a natural square root - the square root.
- * If the number does not have a natural square root - -1.
  */
 
 int _sqrt_recursion(int n)
 {
 	int root = 0;
+
 	if (n < 0)
 		return (-1);
 	if (n == 1)
