53c169a0dfa39db389d1c33e956fc55402838828
kezzyj <karachidavid31@gmail.com>
2022-05-05 07:57:22 -0700

    working withmalloc and 2d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,32 +1,18 @@
-#include "main.h"                                                                                                                             
+#include "main.h" 
 
-/**                                                                                                                                           
- *
- *  * alloc_grid - Returns a pointer to a 2-dimensional array of                                                                                 
- *
- *   * integers with each element initalized to 0.                                                                                                
- *
- *    * @width: The width of the 2-dimensional array.                                                                                              
- *
- *     * @height: The height of the 2-dimensional array.                                                                                            
- *
- *      * Return: If width <= 0, height <= 0, or the function fails - NULL.                                                                          
- *
- *       * Otherwise - a pointer to the 2-dimensional array of integer                                                                                
- *
- *        */                                                                                                                                           
-
-
+/**
+ * alloc_grid - Returns a pointer to a 2-dimensional array of
+ * integers with each element initalized to 0.
+ * @width: The width of the 2-dimensional array.
+ * @height: The height of the 2-dimensional array.
+ * Return: If width <= 0, height <= 0, or the function fails - NULL.
+ * Otherwise - a pointer to the 2-dimensional array of integer
+ */
 
 int **alloc_grid(int width, int height)                                                                                                       
-
-{                                                                                                                                             
-
-	int **twoD;                                                                                                                           
-
+{ 
+	int **twoD;
 	int hgt_index, wid_index;                                                                                                             
-
-
 
 	if (width <= 0 || height <= 0)                                                                                                        
 
