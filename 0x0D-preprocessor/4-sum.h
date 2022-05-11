afc8e074e0552eaeafbc270eb0cc7455d49eb5b3
kezzyj <karachidavid31@gmail.com>
2022-05-11 08:27:14 -0700

    summation

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,13 @@
 #ifndef SUMMATION 
 #define SUMMATION 
 
+/*
+ * File: 4-sum.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ * that computes the sum of the numbers x and y.
+ */
+
 #define SUM(x, y) ((x) + (y))
 
 #endif
