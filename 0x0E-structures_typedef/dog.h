13ba11f4adb95bbb786614c7c29380777989a1fd
kezzyj <karachidavid31@gmail.com>
2022-05-12 04:59:41 -0700

    using typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,17 @@
  * @owner: The owner of the dog.
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
+/**
+* dog_t - Typedef for structure
+*/
+
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
