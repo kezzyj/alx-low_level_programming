7331d17ef91ad8a6c4bea239cb80e76920653595
kezzyj <karachidavid31@gmail.com>
2022-05-12 04:34:47 -0700

    printing struc items

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,16 +10,29 @@ void print_dog(struct dog *d)
 	char *empty = "nil";
 	if (d != NULL)                                                  
 	{                                               
-		if ((*d).name == NULL)  
-			printf("Name: %s\n", empty); 
+		if ((*d).name == NULL)
+		{	
+			printf("Name: %s\n", empty);
+		}
+		else
+		{	
 		printf("Name: %s\n", (*d).name);
-
-		if ((*d).age < 0 )  
-			printf("Age: %s\n", empty); 
-		printf("Age: %f\n", (*d).age);  
-
-		if ((*d).owner == NULL)    
-			printf("Owner: %s\n", empty); 
-		printf("Owner: %s\n", (*d).owner);     
+		}
+		if ((*d).age < 0 )
+		{	
+			printf("Age: %s\n", empty);
+		}
+		else
+		{	
+		printf("Age: %f\n", (*d).age);
+		}	
+		if ((*d).owner == NULL)
+		{	
+			printf("Owner: %s\n", empty);
+		}
+		else
+		{	
+		printf("Owner: %s\n", (*d).owner);
+		}	
 	}                                                            
 }  
