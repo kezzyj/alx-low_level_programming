5bad3311db9a388a5f05abf285c0630335195ec3
kezzyj <karachidavid31@gmail.com>
2022-05-12 04:51:48 -0700

    printing struct items

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,28 +13,18 @@ void print_dog(struct dog *d)
 	if (d != NULL) 
 	{
 		if ((*d).name == NULL)
-		{
 			printf("Name: %s\n", empty);
-		}
 		else
-		{
 		printf("Name: %s\n", (*d).name);
-		}
+
 		if ((*d).age < 0 )
-		{
 			printf("Age: %s\n", empty);
-		}
 		else
-		{
 		printf("Age: %f\n", (*d).age);
-		}
+
 		if ((*d).owner == NULL)
-		{
 			printf("Owner: (nil)\n");
-		}
 		else
-		{	
 		printf("Owner: %s\n", (*d).owner);
-		}	
 	} 
 } 
