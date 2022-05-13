4067d3fda3e303be6c99982f2f0b58924880b079
kezzyj <karachidavid31@gmail.com>
2022-05-13 09:49:35 -0700

    calling a function pointer to perform a task on elements

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,3 +7,14 @@
  * @size: The size of array
  * @action: A pointer to the function to be executed.
  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+	
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i <size; i++)
+		action(array[i] *array[i]);
+}
