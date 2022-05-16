10d49cef9a31df55fa83c03a23352f484a66f3d9
kezzyj <karachidavid31@gmail.com>
2022-05-16 00:58:12 -0700

    searcing intergers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,18 +11,31 @@
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
+
 {
+
 	int index;
 
+
+
 	if (array == NULL || cmp == NULL)
-		return;
-	if (size <= 0 )
+
 		return (-1);
+
+
+
 	for (index = 0; index < size; index++)
+
 	{
-		if (cmp(array[index] != 0)
-				return (index);
-		return (-1);
+
+		if (cmp(array[index]) != 0)
+
+			return (index);
+
 	}
+
+
+
+	return (-1);
 
 }
