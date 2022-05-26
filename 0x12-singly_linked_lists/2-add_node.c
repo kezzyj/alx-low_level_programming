29b65aa605e35841e45ff7f36c367b466b241c4f
kezzyj <karachidavid31@gmail.com>
2022-05-26 07:32:49 -0700

    adding node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,12 +7,12 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (new == NULL)
 		return (NULL);
-	for (; strlen(str); str++)
-	{
+	for (; strlen(str); len++)
+
 	new -> str = strdup(str);
 	new -> len = len;
 	new -> next = *head;
-	*head = str;
-	}
+	*head = new;
+
 	return (new);
 }
