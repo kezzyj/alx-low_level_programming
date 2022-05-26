7fbd818ec8ce25f6466b9993f7b5fd29a0bb1842
kezzyj <karachidavid31@gmail.com>
2022-05-26 05:17:40 -0700

    printing number f elements of a list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,10 +8,10 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t length = 0;                                                                                                        
+	size_t length;                                                                                                        
 	const list_t *items = h;
 
-	for (; items != NULL; length++)
+	for (length = 0; items != NULL; length++)
 		items = items->next;
 	return (length);
 }
