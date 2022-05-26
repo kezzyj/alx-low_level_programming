595c7278ded42d097a9384749b3048c519fc1e05
kezzyj <karachidavid31@gmail.com>
2022-05-26 06:30:06 -0700

    creating a node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int len = 0;
+	list_t new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+	for (; strlen(str); str++)
+	{
+	new -> str = strdup(str);
+	new -> len = len;
+	new -> next = *head;
+	head = str;
+	}
+}
