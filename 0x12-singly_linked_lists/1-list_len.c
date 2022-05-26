d9b7d9644aea120d7a0c894f31cac3afb55affa8
kezzyj <karachidavid31@gmail.com>
2022-05-26 05:11:20 -0700

    finding length of items for node

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,9 +5,8 @@ size_t list_len(const list_t *h)
 {
 	size_t length = 0;                                                                                                        
 	const list_t *items = h;
+
 	for (; items != NULL; length++)
-	{
 		items = items->next;
-	}
 	return (length);
 }
