47c2b3435b7562c276aa91c2b16bfe4054c02f01
kezzyj <karachidavid31@gmail.com>
2022-05-27 02:06:39 -0700

    free nodes

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,24 +1,20 @@
 #include "lists.h"
 
 /**
- *
- *  * free_list - frees a linked list
- *
- *   * @head: a pointer to the first struct in the linked list
- *
- *    * Return: No Value
- *
- *     */
+ * free_list - frees a linked list
+ * @head: a pointer to the first struct in the linked list
+ * Return: No Value
+ */
 
 void free_list(list_t *head)
 {
 	list_t *first;
 
-	for (; head;)
+	for (; head != NULL;)
 	{
+		first = head;
 		head = head->next;
-		free(first->str);
-		free(head);
+		free(first);
 	}
 
 }
