06f9e1606947805db75bd986a42134c878c41faa
kezzyj <karachidavid31@gmail.com>
2022-05-30 08:21:33 -0700

    decimal to binary convertion

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+unsigned int binary_to_uint(const char *b)
+{
+
+	unsigned int ret = 0;
+
+	int i, j;
+
+
+
+	if (b == NULL)
+
+		return (0);
+
+	for (i = 0; b[i + 1]; i++)
+	{
+
+		if (b[i] != '0' || b[i] != '1')
+
+			return (0);
+
+	}
+
+	for (j = 1; i >= 0; i--, j *= 2)
+
+		ret += ((b[i] - '0') * (j));
+
+	return (ret);
+
+}
