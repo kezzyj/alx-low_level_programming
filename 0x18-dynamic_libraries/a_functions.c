0b1ae9c79aefb1b414cd69fbeede44bfbb4c17b1
kezzyj <karachidavid31@gmail.com>
2022-07-12 08:25:02 -0700

    creating a dynamic lib for python

--- 0x18-dynamic_libraries/a_functions.c
+++ 0x18-dynamic_libraries/a_functions.c
@@ -20,7 +20,7 @@ int mul(int a, int b)
 {
 	return(a * b);
 }
-int _div(int a, int b)
+int div(int a, int b)
 {
 	return(a / b);
 }
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -52,7 +52,7 @@ int add(int a, int b);
 
 int sub(int a, int b);
 
-int _div(int a, int b);
+int div(int a, int b);
 
 int mod(int a, int b);
 
